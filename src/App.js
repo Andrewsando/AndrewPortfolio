72807b26f676c2eb66b11b1c6772f0e2480cdc1c
Andres Torres <andres.torressandoval@hotmail.com>
2023-01-02 15:26:54 -0500

    React init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { PrimerComponente } from './components/PrimerComponente';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    
+       
+      <PrimerComponente/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PrimerComponente.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const PrimerComponente = () => {
+  return (
+    <div>
+        < span className="font-link">
+        <h1><strong>¡Hello!</strong></h1>
+        <h1>I'm  Andres Torres Sandoval</h1>
+        <h1>I'm from Bogota, Colombia</h1> 
+        </span>   
+    </div>
+    
+  )
+};
+
+
+
+
